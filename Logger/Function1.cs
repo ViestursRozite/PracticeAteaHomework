8f22864c4ddf4dec4933022768ae171fd6bbf0d3
ViestursRozite <83768961+ViestursRozite@users.noreply.github.com>
2022-01-27 14:24:16 +0200

    Task01 Done

--- Logger/Function1.cs
+++ Logger/Function1.cs
@@ -1,16 +1,73 @@
 using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using System.IO;
+using System.Globalization;
 
-namespace Logger
+namespace MonitorPublicapisOrg
 {
-    public static class Function1
+    class AzureFunction
     {
-        [FunctionName("Function1")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        private static readonly HttpClient client = new HttpClient();
+
+        private static string HttpResponseToLog(HttpResponseMessage response, string readContent)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            string result = "";
+            string n = "\n";
+
+            result += response.ToString() + n + n;
+            result += readContent;
+            result += response.TrailingHeaders + n;
+            result += response.RequestMessage + n;
+
+            return result;
+        }
+
+        public static Stream StringToStream(string s)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+
+            return stream;
+        }
+
+        [FunctionName("LogPublicapisOrgRandom")]
+        public static async Task RunAsync([TimerTrigger("* * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            string blobNameContainingTime = DateTime.Now.ToString("yyyy/MM/dd/HH'-'mm", CultureInfo.InvariantCulture) + ".txt";//.txt files devided up in folders yyyy/MM/dd/
+            string connectionToStorageString = "UseDevelopmentStorage=true";
+            string storageContainerName = "log-publicapis-org-random";
+            string site = @"https://api.publicapis.org/random?auth=null";
+            string wholeHttpResponseAsString;
+
+            //answer from site
+            HttpResponseMessage httpResponse = await client.GetAsync(site);
+
+            //fill httpResponseAsString based on response code 
+            try
+            {
+                var resp = httpResponse.EnsureSuccessStatusCode();//status code == OK, else err
+                string responseBody = await httpResponse.Content.ReadAsStringAsync();
+                wholeHttpResponseAsString = HttpResponseToLog(httpResponse, responseBody);
+            }
+            catch (HttpRequestException exption)
+            {
+                wholeHttpResponseAsString = exption.ToString();//save err message
+            }
+
+            //upload log
+            Stream textToUpoadAsStream = StringToStream(wholeHttpResponseAsString);
+
+            BlobContainerClient blobContainerClient = new BlobContainerClient(connectionToStorageString, storageContainerName);
+            blobContainerClient.CreateIfNotExists();//ensure container exists in Azure
+            blobContainerClient.UploadBlob(blobNameContainingTime, textToUpoadAsStream);//send
         }
     }
 }
